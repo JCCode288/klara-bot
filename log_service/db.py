1a1a9b9553fdfd05f713bf1f462fac63cff356a8
JC <jendycaprico@gmail.com>
2026-01-06 23:54:05 +0700

    feat/time-bucket
    - feat(logger): change query for time bucketing add song
    - feat(logger): change query for time bucketing listening song
    - chore(bot): change error log to string error log
    - fix(bot): reverting leave method to not clearing queue

--- log_service/db.py
+++ log_service/db.py
@@ -22,6 +22,8 @@ class Neo4j:
     @staticmethod
     def _create_song_graph(tx, data):
         query = """
+        WITH datetime().year + "-W" + datetime().week AS current_week
+
         MERGE (u:User {id: $user_id})
         ON CREATE SET u.name = $user_name
         ON MATCH SET u.name = $user_name
@@ -34,7 +36,10 @@ class Neo4j:
         ON CREATE SET g.name = $guild_name
         ON MATCH SET g.name = $guild_name
         
-        MERGE (u)-[:ADDED]->(s)
+        MERGE (u)-[r:ADDED {bucket: current_week}]->(s)
+        ON CREATE SET r.count = 1, r.first_added = timestamp()
+        ON MATCH SET r.count = r.count + 1, r.last_added = timestamp()
+
         MERGE (u)-[:IN_GUILD]->(g)
         
         FOREACH (tag_name IN $song_tags |
@@ -48,6 +53,8 @@ class Neo4j:
     @staticmethod
     def _create_song_listened_graph(tx, data):
         query = """
+        WITH datetime().year + "-W" + datetime().week AS current_week
+
         MERGE (s:Song {url: $song_url})
         ON CREATE SET s.title = $song_title, s.added_at = timestamp(), s.url = $song_url
         ON MATCH SET s.title = $song_title
@@ -60,7 +67,11 @@ class Neo4j:
             MERGE (u:User {id: member.id})
             ON CREATE SET u.name = member.name
             ON MATCH SET u.name = member.name
-            MERGE (u)-[:LISTENED]->(s)
+           
+            MERGE (u)-[r:LISTENED {bucket: current_week}]->(s)
+            ON CREATE SET r.count = 1, r.first_listened = timestamp()
+            ON MATCH SET r.count = r.count + 1, r.last_listened = timestamp()
+
             MERGE (u)-[:IN_GUILD]->(g)
         )
         """
